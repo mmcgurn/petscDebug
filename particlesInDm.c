403e81bf005b19482fa3a1ced7229ab6fb3c98c9
Matt McGurn <75279702+mmcgurn@users.noreply.github.com>
2023-04-21 15:37:29 -0400

    debug example

--- particlesInDm.c
+++ particlesInDm.c
@@ -6,10 +6,10 @@ int main(int argc, char **argv)
 {
     PetscCall(PetscInitialize(&argc, &argv, NULL, help));
 
-    PetscInt dimensions  = 2;
-    PetscInt faces[3] = {20,20};
-    PetscReal lower[3] = {0.0, 0.0};
-    PetscReal upper[3] = {1.0, 1.0};
+    PetscInt dimensions  = 3;
+    PetscInt faces[3] = {200,200, 200};
+    PetscReal lower[3] = {0.0, 0.0, 0.0};
+    PetscReal upper[3] = {1.0, 1.0, 10.0};
     DMBoundaryType bc[3] = {DM_BOUNDARY_NONE, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE};
 
     DM dm;
